da6be6a70f6113fc1a7b84f651d7f5a1fb967859
Lance R. Vick <lance@lrvick.net>
2011-01-25 14:27:50 -0500

    some ui fixes

--- static/js/kralit.js
+++ static/js/kralit.js
@@ -4,6 +4,14 @@ function status_update(msg){
     $("<li class=\"" + msg["service"] + "\"><img src=\"" + msg['user']["avatar"] + "\"/>" + msg["date"] + " | " + msg["user"]['name'] + " : " + msg["text"] + "</li>").prependTo("#microblogs ul");
     $('#microblogs li:last').remove();
 }
+function video_update(msg){
+    $("<li class=\"" + msg["service"] + "\" ><img src='"  + msg['thumbnail']  +  "' /><a href=\"#foo\">" + msg["text"] + "</a></li>").prependTo("#videos ul");
+    $('#videos li:last').remove();
+}
+function picture_update(msg){
+    $("<li class=\"" + msg["service"] + "\" style=\"background:url('"  + msg['thumbnail']  +  "') center\"><a href=\"#foo\">" + msg["text"] + "</a></li>").prependTo("#pictures ul");
+    $('#pictures li:last').remove();
+}
 onload = function() {
     stomp = new STOMPClient();
     stomp.connect('localhost', 61613,"guest","guest");
@@ -21,10 +29,14 @@ onload = function() {
         console.log('Connected');
     };
     stomp.onmessageframe = function(frame){
-        //console.log(frame.body);
+        console.log(frame.body);
         msg = JSON.parse(frame.body);
         if (msg['service'] in {'twitter':'', 'facebook':'', 'identica':''}) {
             status_update(msg);
+        } else if (msg['service'] in {'youtube':''}) {
+            video_update(msg);
+        } else if (msg['service'] in {'flickr':''}) {
+            picture_update(msg);
         }
     };
 };
